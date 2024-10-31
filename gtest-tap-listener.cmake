9d6edd257a04cb0997aaa422df40e8679c006308
James Cahill <jcahill773@gmail.com>
2024-10-31 15:07:09 +0000

    Revert to pre-gtest-tap-listener codebase

--- gtest-tap-listener.cmake
+++
@@ -1,5 +0,0 @@
-project(gtest-tap-listener VERSION 0.7 LANGUAGES CXX)
-
-add_library(gtest-tap-listener INTERFACE)
-
-target_include_directories(gtest-tap-listener INTERFACE lib/google/gtest-tap-listener/src)
