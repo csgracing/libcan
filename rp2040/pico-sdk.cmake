6026cc18e94186248786654da2cf6b6b4c79acb2
James Cahill <jcahill@ieee.org>
2025-03-10 16:01:03 +0000

    (chore): Add initial doxygen support, reorganise directory structure, enable PIC for all targets (#4)
    * (lib/docs): Add doxygen theme submodule
    
    * (docs): Initial doxyfile setup
    
    * (lib): Reorganise submodules under top-level lib/
    
    * (docs): Set build_doc option to default false
    
    * (chore): Attempt to re-organize directory structure
    
    * (root/cmake): Fix test subdir
    
    * (ci/github): Attempt caching of toolchain and build deps
    
    * (ci/github): Add exec install scripts to cached installer
    
    * Revert "(ci/github): Add exec install scripts to cached installer"
    
    This reverts commit dca6319a7a86d3bf7d4203e4a8f162419814c130.
    
    * (docs/cmake): Only setup doxygen if flag set
    
    * (root/cmake): Manually set gcc when not fuzzing
    
    * (ci/github): Only install clang for fuzzing job
    
    * (ci/github/test-fuzz): Fix syntax
    
    * Revert "(ci/github): Attempt caching of toolchain and build deps"
    
    This reverts commit 965def9d68d3568e15f87d01b75d5699698a40d5.
    
    * (root/cmake): enable PIC for all targets
    
    * (root/cmake): Re-add test/core subdirectory after move

--- rp2040/pico-sdk.cmake
+++
@@ -1,3 +0,0 @@
-set(PICO_SDK_PATH ${CMAKE_CURRENT_LIST_DIR}/lib/pico-sdk)
-
-include(${PICO_SDK_PATH}/external/pico_sdk_import.cmake)
---
+++ src/rp2040/pico-sdk.cmake
@@ -0,0 +1,3 @@
+set(PICO_SDK_PATH ${CMAKE_SOURCE_DIR}/lib/rp2040/pico-sdk)
+
+include(${PICO_SDK_PATH}/external/pico_sdk_import.cmake)
