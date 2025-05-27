15146fd07dd39abf261b019202ca820c3578b92a
James Cahill <jcahill@ieee.org>
2025-05-27 16:16:44 +0100

    (ci/github/socketcan): Misc fixes to workflow, refactor testing script

--- .github/workflows/scripts/socketcan-run-tests.sh
+++ .github/workflows/scripts/socketcan-run-tests.sh
@@ -1,18 +1,64 @@
+#!/bin/bash
+
+# $1 - pwd
+# $2 - path from pwd to test binary
+
+port=10022
+host=root@localhost
+
+get_params() {
+    echo "-oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+}
+
+# do_ssh "<cmd>"
+do_ssh () {
+    sshpass -proot ssh -q -p$port $(get_params) $host -t  $1
+}
+
+do_scp ()  {
+    sshpass -proot scp -P$port $(get_params) $1 $2
+}
+
+echo "::group::vm: booting virtual machine"
 
 cd build/sil-kit
-./tools/run-silkit-qemu-demos-guest.sh &
 
+# start vm in background, note that the image will verbose log kernel messages!
+./tools/run-silkit-qemu-demos-guest.sh & 
+
+pid = $!
 
 # wait for system to be up
-ssh -q -p10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@localhost -p root
+do_ssh "echo success"
 while test $? -gt 0
 do
    sleep 5
-   ssh -q -p10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@localhost -p root
+   echo "unable to connect. sleeping for 5 seconds..."
+   # wait for system to be up
+   do_ssh "echo success"
 done
 
-sshpass -proot ssh -q -p10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@localhost -p root -t "mkdir -p /work" || :
-sshpass -proot scp -P10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null $1 root@localhost:/work/test || :
-sshpass -proot ssh -q -p10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@localhost -p root -t "cd /work && ./test" || :
+echo "::endgroup::"
+echo "::group::vm: copying test binary"
 
-sshpass -proot ssh -q -p10022 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@localhost -q -t "shutdown 00"
+do_ssh "mkdir -p /work" || :
+do_scp $1/$2 $host:/work/test || :
+
+echo "::endgroup::"
+echo "::group::vm: executing test binary"
+
+do_ssh "cd /work && ./test --gtest_output=xml" || :
+
+echo "::endgroup::"
+echo "::group::vm: copying test results"
+
+do_scp $host:/work/test_detail.xml $1/ || :
+
+echo "::endgroup::"
+echo "::group::vm: shutting down virtual machine"
+
+do_ssh "shutdown 00"
+
+wait $pid
+
+echo "::endgroup::"
